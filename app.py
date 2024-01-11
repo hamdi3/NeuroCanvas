d3588b711746003090df82fc5446e53938adb098
Hamdi <Hamdi3khalil@gmail.com>
2024-01-11 12:18:36 +0100

    ADD
    Added an about and report an issue components to the menu and added a centered title to the app

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from utils.menu import render_menu
 from utils.visualize_model import visualize_model
 
 # Define page config
-st.set_page_config(page_title="NeuroCanvas", page_icon="🎨"
-                  , layout="wide", initial_sidebar_state="auto")
+st.set_page_config(page_title="NeuroCanvas", page_icon="🎨", layout="wide", initial_sidebar_state="auto",
+                   menu_items={
+                    'Report a bug': "https://github.com/hamdi3/NeuroCanvas/issues",
+                    'About': "# 🎨 :rainbow[NeuroCanvas]\n This is an app made to help you easily **Create**, **visualize** and **Train** neuronal networks. \n \n Any suggestions are welcomed [here]('https://github.com/hamdi3/NeuroCanvas/issues')."
+                    })
 
 # Initialize an empty list to store the layers
 if "layers" and "G" not in st.session_state:
@@ -15,6 +18,11 @@ if "layers" and "G" not in st.session_state:
 
 
 def main():
+    # For centering the title
+    _,colT2 = st.columns([1,2.2])
+    with colT2:
+        st.title("🎨 :rainbow[NeuroCanvas]")
+
     # Render the selection menu
     render_menu()
 
